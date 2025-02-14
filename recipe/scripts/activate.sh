bbb4ce22eb32c72ca0c970d9e22cc6b6ba8e2106
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-02-14 09:50:07 +0100

    condition on CONDA_BUILD

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,2 +1,4 @@
-export CLI_NO_THREAD_CONDA_BACKUP="${CLI_NO_THREAD:-}"
-export CLI_NO_THREAD=1
+if [[ "${CONDA_BUILD:-}" == "1" ]]; then
+  export CLI_NO_THREAD_CONDA_BACKUP="${CLI_NO_THREAD:-}"
+  export CLI_NO_THREAD=1
+fi
